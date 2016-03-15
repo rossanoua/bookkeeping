d73001ef6c9c6b4ff2144a3fe3d2cd75f4d8b9a8
Ruslan Kvashuk <raidho26@gmail.com>
2016-03-15 23:24:20 +0100

    commit my next commit, for counting, nothing special, just commit if ajax start to work I merge  branches in master

--- js/data_insert.js
+++ js/data_insert.js
@@ -1,9 +1,26 @@
-$( document ).ready( function(){
-    
+$(document).ready(function () {
+
     //this will fire once the page has been fully loaded
-    
-    $( '#data-post-btn' ).click( function(){
-        console.log( "button is working just fine" );
+
+    $('#data-post-btn').click(function () {
+
+        //data inserted in the gas area
+        var _gas_data = $('#post-gas-data').val();
+        //data inserted in the water area
+        var _water_data = $('#post-water-data').val();
+        //data inserted in the ellectro area
+        var _ellectro_data = $('#post-ellectro-data').val();
+
+
+        if (_gas_data.length > 0 && _water_data.length > 0 && _ellectro_data.length > 0) {
+
+            console.log(_gas_data + " " + _water_data + " " + _ellectro_data);
+        }
+
+
+        $('#post-gas-data').val("");
+        $('#post-water-data').val("");
+        $('#post-ellectro-data').val("");
     });
-    
+
 });
